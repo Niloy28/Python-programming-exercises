2c2c635f3525f30e6ff44f01f293299e1a87b3f4
Niloy28 <Niloy28@live.com>
2019-07-01 01:38:01 +0600

    Update Q7.py

--- Solutions/Q7.py
+++ Solutions/Q7.py
@@ -1,12 +1,17 @@
 input_str = input()
 dimensions = [int(x) for x in input_str.split(',')]
 
-x = dimensions[0]
-y = dimensions[1]
+row = dimensions[0]
+col = dimensions[1]
 
-array = [[0 for i in range(0, y)] for j in range(0, x)]
-for i in range(0, x):
-    for j in range(0, y):
+# conventional method
+array = [[0 for i in range(0, col)] for j in range(0, row)]
+for i in range(0, row):
+    for j in range(0, col):
         array[i][j] = i * j
 
+# using list comprehension
+multilist = [i * j for i in range(0, col)] for j in range(0, row)]
+
 print(array)
+print(multilist)
